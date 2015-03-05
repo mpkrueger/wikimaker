931b741588d165b4217dda61838db1eda05e4e98
mpkrueger <mpkrueger@gmail.com>
2015-03-04 22:01:49 -0800

    added devise and got first authentication test for sign up to pass

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "Sign up flow" do
+
+  describe "successful" do
+    it "takes a valid email address" do
+      visit root_path
+
+      click_link 'Sign Up'
+      fill_in 'Email', with: "matt@example.com"
+      fill_in 'Password', with: "password"
+      click_button "Sign up"
+
+      expect(current_path).to eq user_registration_path
+
+    end
+  end
+  
+end
