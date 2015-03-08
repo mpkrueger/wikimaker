1fecba0f7240e3ff33753beb4eed04d162e89181
mpkrueger <mpkrueger@gmail.com>
2015-03-07 22:13:50 -0800

    minor updates to signed in user's index view

--- spec/features/authentication.rb
+++ spec/features/authentication.rb
@@ -15,8 +15,21 @@ describe "Sign up flow" do
       expect(current_path).to eq user_registration_path
 
     end
-
     
+  end
+
+  describe "errors" do
+
+    it "returns an error if duplicate email address" do
+      visit root_path
+      click_link 'Sign Up'
+      fill_in 'Email', with: "matt@example.com"
+      fill_in 'Password', with: "password"
+    end
+
+    it "returns an error if passwords don't match" do
+    end
+
   end
   
 end
