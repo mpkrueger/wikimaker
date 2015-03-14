d909d3e936f24bbaffa712c7d052656b03457271
mpkrueger <mpkrueger@gmail.com>
2015-03-14 13:52:59 -0700

    Added faker gem and set up seed for users and wikis

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create Users
+10.times do
+  User.create!(
+    name: Faker::Name.first_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    role: ["standard", "admin", "premium"].sample
+  )
+end
+users = User.all
+
+# Create Wikis
+25.times do
+  Wiki.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph,
+    user: users.sample,
+    private: [true, false].sample
+  )
+end
+wikis = Wiki.all
+
+puts "Seed finished"
+puts "#{User.count} users created."
+puts "#{Wiki.count} wikis created."
